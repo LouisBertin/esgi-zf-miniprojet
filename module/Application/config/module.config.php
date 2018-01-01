76a19aaf008ff577eec2fb8c574bd2fcf0964325
LouisBertin <bertin.louis7@gmail.com>
2018-01-01 18:01:27 +0100

    require doctrine w/ composer and configure it

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -60,4 +60,25 @@ return [
             __DIR__ . '/../view',
         ],
     ],
+    'doctrine' => [
+        'driver' => [
+            // defines an annotation driver with two paths, and names it `my_annotation_driver`
+            'application_driver' => [
+                'class' => \Doctrine\ORM\Mapping\Driver\AnnotationDriver::class,
+                'cache' => 'array',
+                'paths' => [
+                    __DIR__.'/../src/Entity/',
+                ],
+            ],
+
+            // default metadata driver, aggregates all other drivers into a single one.
+            // Override `orm_default` only if you know what you're doing
+            'orm_default' => [
+                'drivers' => [
+                    // register `application_driver` for any entity under namespace `Application\Entity`
+                    'Application\Entity' => 'application_driver',
+                ],
+            ],
+        ],
+    ],
 ];
